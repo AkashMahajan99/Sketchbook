cc889128860afdb08bdd0affe5d0a5a42cfba2cf
Akash Mahajan <akkim3899@gmail.com>
2023-11-06 18:55:12 +0530

    be endpoint link updated

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? 'https://sketchbook-server-6hj5.onrender.com' : 'http://localhost:5000'
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketchbook-server-qcbu.onrender.com' : 'http://localhost:5000'
 
 export const socket = io(URL);
 
